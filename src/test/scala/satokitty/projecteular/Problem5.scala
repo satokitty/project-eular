28e59dab68629e0d464e0c98dee6008cdd8979e9
satokitty <tomohiko.s@gmail.com>
2013-01-06 08:04:10 +0900

    solved problem 5

---
+++ src/test/scala/satokitty/projecteular/Problem5.scala
@@ -0,0 +1,39 @@
+package satokitty.projecteular
+
+import org.scalatest.FunSuite
+
+/**
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+class Problem5 extends FunSuite {
+  // lcm(m,n) = abs(m*n) / gcd(m, n)
+  // lcm(l,m,n) = lcm(lcm(m,n),l)
+
+  /*
+   * m > n, m > 0, n >= 0
+   */
+  def gcd(m:Long, n:Long):Long = n match {
+    case 0 => m
+    case _ => gcd(n, m % n)
+  }
+
+  /*
+   * nums must be orderd by desc.
+   */
+  def lcd(nums: Seq[Long]): Long = nums match {
+    case a::Nil => a
+    case _ => nums.tail.foldLeft(nums.head)((m, n) => m * n / gcd(m, n))
+  }
+
+  test("sample") {
+    assert(lcd((1L to 10L).reverse) === 2520)
+  }
+
+  test("solution") {
+    val n = lcd((1L to 20L).reverse)
+    (1 to 20).foreach(i => { println(i); assert((n % i) === 0)})
+    println("answer is " + n)
+  }
+}
